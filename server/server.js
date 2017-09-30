eeb225053a773d4ec51406b4be1ec84ad5b055c7
adria mooney <adriamooney@gmail.com>
2017-09-29 19:27:42 -0700

    added tests

--- server/server.js
+++ server/server.js
@@ -20,10 +20,23 @@ app.post('/todos', (req, res) => {
 	todo.save().then((doc) => {
 		res.send(doc)
 	}, (e) => {
-		res.status(400).res.send(e);
+		res.status(400).send(e);
 	});
 });
+
+//get todos
+
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos)=> {
+		res.send({todos});
+	}, (e) => {
+		res.status(400).send(e);
+	});
+});
+
 
 app.listen(3000, () => {
 	console.log('started on port 3000')
 });
+
+module.exports = {app};
